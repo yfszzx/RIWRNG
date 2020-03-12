33c8eb17c0f1f270fa8ee8ebc35e0d3c4a540f0e
ubuntu <geek@ubuntu>
2020-03-12 14:00:49 +0800

    database

--- main/models.py
+++ main/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 
 class user(models.Model):
-    name = models.CharField(null=True, max_length=100)
+    openid  = models.CharField(null=True, max_length=100)
+    nickname = models.CharField(null=True, max_length=200)
+    sex = models.BooleanField(default=False) 
+    headimgurl = models.CharField(null=True, max_length=1000)
+    create_timestamp = models.DateTimeField(auto_now_add=True) 
+    grade = models.SmallIntegerField(null=True, default=0)
+
+class score(models.Model):
+    user = models.ForeignKey(user, db_index=True, on_delete=models.CASCADE)
+    train_num = models.BigIntegerField(null=True, default=0)
+    train_score = models.BigIntegerField(null=True, default=0)
+    verify_num = models.BigIntegerField(null=True, default=0)
+    verify_score = models.BigIntegerField(null=True, default=0)
+    exp_num = models.BigIntegerField(null=True, default=0)
+    exp_score = models.BigIntegerField(null=True, default=0)
+
 
 class experiment(models.Model):
     user = models.ForeignKey(user, db_index=True, on_delete=models.CASCADE)
-    num = models.SmallIntegerField(null=True, default=0)
+    exp_mod =  models.CharField(null=True, max_length=1) # 't': train , 'v':verify, 'e':experiment
+    rnd_num = models.SmallIntegerField(null=True, default=0)
+    exp_score= models.SmallIntegerField(null=True, default=0)
+    compare_score = models.SmallIntegerField(null=True, default=0)
+    research_score = models.SmallIntegerField(null=True, default=0)
+    create_timestamp = models.DateTimeField(auto_now_add=True) 
 
 
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests as rq
+import json
 
-# Create your views here.
+def main(request):
+    code  = request.GET['code']
+    r = rq.get(f"https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx6742e22952f55f43&secret=7dc332250bc57fa4c8a46f980d112956&code={code}&grant_type=authorization_code")
+    openid = json.loads(r.text)["openid"]
+    return HttpResponse(openid)
+
+
